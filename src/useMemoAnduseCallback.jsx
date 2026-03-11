24cfcb24120ee657186490281d3c0be3a4a32542
sagarmj94 <sagarmj1502@gmail.com>
2026-03-11 22:00:24 +0530

    use memo example and answers

--- src/useMemoAnduseCallback.jsx
+++ src/useMemoAnduseCallback.jsx
@@ -1,4 +1,5 @@
 import { useMemo, useState } from "react";
+import "./useMemo.css";
 
 const UseMemoAnduseCallback = () => {
   const [counter, setCounter] = useState(0);
@@ -10,100 +11,63 @@ const UseMemoAnduseCallback = () => {
   }, [counter]);
 
   return (
-    <>
-      <div>
-        <h3 style={{ textAlign: "center" }}>useMemoAnduseCallback</h3>
-      </div>
-      <h5
-        style={{
-          color: "#1E40AF",
-          backgroundColor: "#DBEAFE",
-          padding: "6px",
-          borderRadius: "5px",
-        }}
-      >
-        Question 1: What is useMemo in React?
-      </h5>
-      <h6
-        style={{
-          color: "#166534",
-          backgroundColor: "#DCFCE7",
-          padding: "6px",
-          borderRadius: "5px",
-        }}
-      >
-        -It is a hook used to memorize the result of a function and cache it,
-        recalculating it only if the depedencies change.
-      </h6>
-      <h2
-        style={{
-          color: "#065F46",
-          backgroundColor: "#ECFDF5",
-          padding: "6px",
-          borderRadius: "5px",
-        }}
-      >
-        Square Counter: {squareValue}
-      </h2>
-      <button
-        style={{
-          backgroundColor: "#FEF3C7",
-          color: "#92400E",
-          padding: "8px 12px",
-          borderRadius: "5px",
-          border: "none",
-          cursor: "pointer",
-        }}
-        onClick={() => setCounter(counter + 1)}
-      >
-        Increament
-      </button>
-      <h2
-        style={{
-          color: "#991B1B",
-          backgroundColor: "#FEE2E2",
-          padding: "6px",
-          borderRadius: "5px",
-        }}
-      >
-        deSquare Counter2: {counter2}
-      </h2>
-      <button
-        style={{
-          backgroundColor: "#FEE2E2",
-          color: "#991B1B",
-          padding: "8px 12px",
-          borderRadius: "5px",
-          border: "none",
-          cursor: "pointer",
-        }}
-        onClick={() => setCounter2(counter2 - 1)}
-      >
-        Decreament
-      </button>
+    <div className="container">
+      <h1 className="title">useMemo Hook Example</h1>
 
-      <h5
-        style={{
-          color: "#1E40AF",
-          backgroundColor: "#DBEAFE",
-          padding: "6px",
-          borderRadius: "5px",
-          marginTop: "15px",
-        }}
-      >
-        Question 2:When should you use useMemo hook?
-      </h5>
-      <h6
-        style={{
-          color: "#166534",
-          backgroundColor: "#DCFCE7",
-          padding: "6px",
-          borderRadius: "5px",
-        }}
-      >
-        When computingvalue is expensive or time consuming.
-      </h6>
-    </>
+      <div className="example">
+        <h3 className="counter-green">Square Counter: {squareValue}</h3>
+
+        <button
+          className="btn-increment"
+          onClick={() => setCounter(counter + 1)}
+        >
+          Increment
+        </button>
+
+        <h3 className="counter-red">Counter2: {counter2}</h3>
+
+        <button
+          className="btn-decrement"
+          onClick={() => setCounter2(counter2 - 1)}
+        >
+          Decrement
+        </button>
+      </div>
+
+      {/* Question 1 */}
+      <h2 className="question">Question 1: What is useMemo in React?</h2>
+
+      <p className="answer">
+        It is a hook used to memorize the result of a function and cache it,
+        recalculating it only if the dependencies change.
+      </p>
+
+      {/* Question 2 */}
+      <h2 className="question">
+        Question 2: When should you use useMemo hook?
+      </h2>
+
+      <p className="answer">
+        When computing value is expensive or time consuming.
+      </p>
+      <p className="answer">
+        When you want to prevent unnecessary re-computation of values across
+        re-renders.
+      </p>
+      <p className="answer">
+        useMemo hoo does not return a function it will return a{" "}
+        <span className="highlight"> memorized value.</span>
+      </p>
+
+      <h2 className="question">
+        Question 3: How does useMemo differ from useState?
+      </h2>
+      <p className="answer">
+        useMmo memorizes a computed value and returns the cached value without
+        causing re-renders, while useState manages state and triggeres
+        re-renders when the state changes.
+      </p>
+    </div>
   );
 };
 
